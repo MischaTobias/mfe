5599832f424d75d0beae553b5c2fe06372d71920
Mischa Tobias <mischa.tobias4b@gmail.com>
2022-01-11 23:05:41 -0600

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MarketingApp from './components/MarketingApp';
+
+export default () => {
+  return (
+    <h1>
+      Hi there!
+      <hr />
+      <MarketingApp />
+    </h1>
+  );
+};
